80673c8e8cde0701a8c3b546130e9ed1d7bf68b9
moustafa.moungari <moungari.moustafa@hotmail.com>
2021-08-20 18:50:43 +0200

    Creation d'un classe voiture sans pattern

--- src/com/training/Main.java
+++ src/com/training/Main.java
@@ -1,6 +1,7 @@
 package com.training;
 
 import com.training.Appareil.*;
+import com.training.voiture.Voiture;
 
 public class Main {
 
@@ -8,17 +9,24 @@ public class Main {
 	// write your code here
         //ceci sans le pattern State
         AppareilMenager appareil1= new AppareilMenager();
-
-
-
+        Voiture v1 = new Voiture();
+        v1.setInstruction("Demarrer");
+        v1.action();
+        v1.setInstruction("accelerer");
+        v1.action();
+        v1.setInstruction("ralentir");
+        v1.action();
         //Mettons en place le pattern State
-        appareil1.setPosition(new Demarrer());
+/*        appareil1.setPosition(new Demarrer());
         appareil1.action();
         appareil1.setPosition(new Chauffage());
         appareil1.action();
         appareil1.setPosition(new Refroidir());
         appareil1.action();
         appareil1.setPosition(new Stopper());
-        appareil1.action();
+        appareil1.action();*/
+
+
+
     }
 }
---
+++ src/com/training/voiture/Voiture.java
@@ -0,0 +1,26 @@
+package com.training.voiture;
+
+public class Voiture {
+
+    private String instruction ;
+
+    public void setInstruction(String instruction){this.instruction = instruction;}
+
+
+    public  void action(){
+
+        if(instruction.equalsIgnoreCase("demarrer"))
+        {
+            System.out.println("Demarrage du véhicule ");
+        }else if(instruction.equalsIgnoreCase("accelerer")){
+            System.out.println("Acceleration du véhicule");
+        }else if(instruction.equalsIgnoreCase("ralentir")){
+            System.out.println("le véhicule ralentit");
+        }else if(instruction.equalsIgnoreCase("arreter")){
+            System.out.println("le véhicule s'arrete");
+        }
+
+
+    }
+
+}
